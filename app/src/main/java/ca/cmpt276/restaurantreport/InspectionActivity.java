da61a21830502c1ae12e31811ed5af50bca95111
Sam Zhou <2006.tommy@gmail.com>
2020-02-28 19:54:52 -0800

    Added 2 new empty activities Forced portrait orientation on all activities

---
+++ app/src/main/java/ca/cmpt276/restaurantreport/InspectionActivity.java
@@ -0,0 +1,14 @@
+package ca.cmpt276.restaurantreport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InspectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inspection_detail);
+    }
+}
