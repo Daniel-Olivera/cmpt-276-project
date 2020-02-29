00f9811db89330196162aea73f936b444ee84e7c
Sam Zhou <2006.tommy@gmail.com>
2020-02-28 19:56:41 -0800

    edited some names

--- app/src/main/java/ca/cmpt276/restaurantreport/InspectionActivity.java
+++ app/src/main/java/ca/cmpt276/restaurantreport/InspectionActivity.java
@@ -9,6 +9,6 @@ public class InspectionActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_inspection_detail);
+        setContentView(R.layout.activity_inspection);
     }
 }
