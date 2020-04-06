b3d0f3881cff2567ddd463561775c173ca42d2ab
Sam Zhou <2006.tommy@gmail.com>
2020-04-05 17:31:16 -0700

    Removed all LinearLayouts

--- app/src/main/java/ca/cmpt276/restaurantreport/ui/UpdateDialog.java
+++ app/src/main/java/ca/cmpt276/restaurantreport/ui/UpdateDialog.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -30,14 +29,8 @@ public class UpdateDialog extends DialogFragment {
     private ProgressBar progressBar;
     private int progressStatus = 0;
     private Handler handler = new Handler();
-    private ProcessData processData;
     private Context context;
-
     private boolean updateCancelled = false;
-
-    public int getProgressStatus() {
-        return progressStatus;
-    }
 
     UpdateDialog(Context context){
         this.context = context;
@@ -48,23 +41,17 @@ public class UpdateDialog extends DialogFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.update_dialog, container, false);
 
-        progressBar = view.findViewById(R.id.progress_bar);
-        TextView title = view.findViewById(R.id.dialog_title);
+        progressBar = view.findViewById(R.id.progbarUpdateDialog);
         Button cancelButton = view.findViewById(R.id.cancel_button);
 
-        cancelButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                UpdateActivity.clickedCancel = true;
-
-                updateCancelled = true;
-
-                Objects.requireNonNull(getDialog()).dismiss();
-                Intent intent = MapsActivity.makeIntent(context);
-                startActivity(intent);
-            }
+        cancelButton.setOnClickListener(v -> {
+            UpdateActivity.clickedCancel = true;
+            updateCancelled = true;
+            Objects.requireNonNull(getDialog()).dismiss();
+            Intent intent = MapsActivity.makeIntent(context);
+            startActivity(intent);
         });
-        //textView = (TextView) findViewById(R.id.textView);
+
         // Start long running operation in a background thread
         new Thread(new Runnable() {
             public void run() {
@@ -73,11 +60,7 @@ public class UpdateDialog extends DialogFragment {
                     progressStatus += 2;
                     // Update the progress bar and display the
                     //current value in the text view
-                    handler.post(new Runnable() {
-                        public void run() {
-                            progressBar.setProgress(progressStatus);
-                        }
-                    });
+                    handler.post(() -> progressBar.setProgress(progressStatus));
 
                     try {
                         // Sleep for 200 milliseconds.
@@ -89,11 +72,11 @@ public class UpdateDialog extends DialogFragment {
 
                 if(!updateCancelled) {
 
-                        ReadCSV readCSV = new ReadCSV(context,true,0);
+                        new ReadCSV(context,true,0);
                         LocalDateTime currentTime = LocalDateTime.now();
                         String strCurrentTime = currentTime.toString();
                         saveWhenLastUpdated(strCurrentTime);
-                        String dateLastSaved = getWhenLastUpdated(context);
+                        getWhenLastUpdated(context);
                         ProcessData processData = new ProcessData();
                         processData.saveFinalCopy(context);
 
@@ -102,7 +85,6 @@ public class UpdateDialog extends DialogFragment {
                 }
             }
         }).start();
-
         return view;
     }
 
@@ -112,6 +94,4 @@ public class UpdateDialog extends DialogFragment {
         editor.putString("last_updated",lastUpdated);
         editor.apply();
     }
-
-
 }
